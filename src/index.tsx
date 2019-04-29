4a9ddbe8dcc3cc815f4650437209dedb8aeb3940
Kier Borromeo <seraphipod@gmail.com>
2019-04-29 10:59:51 +0800

    Show list

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import './global.css'
 import 'sanitize.css'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { url } from 'inspector';
 
 function App() {
   return (
@@ -18,20 +19,45 @@ function App() {
         </div>
       </div>
 
-      <div className="show-list">
-        <h5 className="ui-subheading">
-          New Releases
-        </h5>
+      {Array(10).fill(0).map((category, i) =>
+        <div className="show-list" key={i}>
+          <h5 className="ui-subheading">
+            New Releases
+          </h5>
 
-        <div className="show-carousel">
-          <div className="inner">
-            <div className="card">
-              <div className="show-carousel-card">
-              </div>
+          <div className="show-carousel">
+            <div className="inner">
+              {Array(10).fill(0).map((show, j) =>
+                <div className="card" key={j}>
+                  <div className="show-carousel-card" style={{ backgroundImage: `url(${require('~/assets/show-thumbnail-218x146.jpg')})` }}>
+                    <div className="overlay" />
+
+                    <div className="details">
+                      <div className="tags">
+                        <span className="tag">
+                          2018
+                        </span>
+
+                        <span className="tag">
+                        •
+                        </span>
+
+                        <span className="tag">
+                          Anime
+                        </span>
+                      </div>
+
+                      <h3 className="title">
+                        My Hero Academia
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
-      </div>
+      )}
     </React.Fragment>
   )
 }

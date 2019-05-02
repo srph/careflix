ee5663311b57ab3ae2d9cf6c6082cd3df18123f0
Kier Borromeo <seraphipod@gmail.com>
2019-05-02 22:36:49 +0800

    Actions

--- src/screens/app.watch.home/PlayerModal/index.tsx
+++ src/screens/app.watch.home/PlayerModal/index.tsx
@@ -11,35 +11,66 @@ function PlayerModal() {
   return (
     <UiModal isOpen={isOpen} shouldCloseOnOverlayClick={false}>
       <div className="watch-player-modal">
-        <div className="watch-player-controls">
-          <div className="control">
-            <UiPlainButton>
-              <i className="fa fa-play" />
-            </UiPlainButton>
+        <div className="watch-player-actions">
+          <div className="section">
+            <div className="action">
+              <UiPlainButton>
+                <i className="fa fa-close" />
+              </UiPlainButton>
+            </div>
           </div>
 
-          <div className="control">
-            <UiPlainButton>
-              <i className="fa fa-fast-backward" />
-            </UiPlainButton>
-          </div>
+          <div className="section">
+            <div className="action">
+              <UiPlainButton>
+                <i className="fa fa-comments" />
+              </UiPlainButton>
+            </div>
 
-          <div className="control">
-            <UiPlainButton>
-              <i className="fa fa-fast-forward" />
-            </UiPlainButton>
+            <div className="action">
+              <UiPlainButton>
+                <i className="fa fa-cc" />
+              </UiPlainButton>
+            </div>
+            <div className="action">
+              <UiPlainButton>
+                <i className="fa fa-cog" />
+              </UiPlainButton>
+            </div>
           </div>
         </div>
 
-        <div className="watch-player-modal-card">
-          <div className="meta">
-            <h6 className="ui-subheading">
-              Season 6: Episode 9
-            </h6>
+        <div className="watch-player-contents">
+          <div className="watch-player-controls">
+            <div className="control">
+              <UiPlainButton>
+                <i className="fa fa-play" />
+              </UiPlainButton>
+            </div>
+
+            <div className="control">
+              <UiPlainButton>
+                <i className="fa fa-fast-backward" />
+              </UiPlainButton>
+            </div>
+
+            <div className="control">
+              <UiPlainButton>
+                <i className="fa fa-fast-forward" />
+              </UiPlainButton>
+            </div>
           </div>
 
-          <h2 className="title">Boku no Hero Academia</h2>
-          <p className="summary">Detective Jake Peralta, a talented and carefree cop with the best arrest record, has never had to follow the rules too closely or work very hard.</p>
+          <div className="watch-player-modal-card">
+            <div className="meta">
+              <h6 className="ui-subheading">
+                Season 6: Episode 9
+              </h6>
+            </div>
+
+            <h2 className="title">Boku no Hero Academia</h2>
+            <p className="summary">Detective Jake Peralta, a talented and carefree cop with the best arrest record, has never had to follow the rules too closely or work very hard.</p>
+          </div>
         </div>
       </div>
     </UiModal>

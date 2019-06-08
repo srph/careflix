1c2c9d67565bad50a296a8400073165094c23b40
srph <seraphipod@gmail.com>
2019-06-09 02:17:48 +0800

    Invitation sound

--- ui/src/screens/app/InvitationModal/index.tsx
+++ ui/src/screens/app/InvitationModal/index.tsx
@@ -9,16 +9,16 @@ import cx from 'classnames'
 import axios from '~/lib/axios'
 import history from '~/lib/history'
 import { usePusher } from '~/hooks/usePusher'
-import { useReducer, useMemo } from 'react'
-import { useCountdownTimer } from '~/hooks/useCountdownTimer'
-import { useInterval } from '~/hooks/useInterval'
+import { useRef, useEffect, useReducer, useMemo } from 'react'
 import { useUnstated } from '~/lib/unstated'
 import { AuthContainer } from '~/containers'
 import getAirDetails from '~/utils/shows/getAirDetails'
 
 import { useNow } from '~/hooks/useNow';
-import { parse, isBefore, differenceInSeconds } from 'date-fns'
+import { parse, isBefore } from 'date-fns'
 import getFormattedRemainingTime from '~utils/date/getFormattedRemainingTime'
+
+import asset_invitation from '~/assets/audio/invitation.ogg'
 
 interface State {
   isAccepting: boolean
@@ -94,6 +94,14 @@ function InvitationModal() {
     }
   )
 
+  const notificationRef = useRef<HTMLAudioElement>()
+
+  useEffect(() => {
+    if (invitation != null) {
+      notificationRef.current.play()
+    }
+  }, [invitation])
+
   async function handleAccept() {
     dispatch({
       type: 'action:init',
@@ -163,76 +171,82 @@ function InvitationModal() {
   }, [now])
 
   return (
-    <UiModal isOpen={invitation != null} shouldCloseOnOverlayClick={false}>
-      {invitation != null && (
-        <div className={cx('invitation-modal', { 'is-cancelled': isInvalid })}>
-          <div className="actions">
-            {isInvalid && (
-              <UiButton onClick={handleCloseCancelled} disabled={isLoading}>
-                Close
-              </UiButton>
-            )}
-
-            {!isInvalid && (
-              <React.Fragment>
-                <UiButton onClick={handleDecline} disabled={isLoading}>
-                  {state.isDeclining ? 'Declining...' : 'Decline'}
+    <React.Fragment>
+      <UiModal isOpen={invitation != null} shouldCloseOnOverlayClick={false}>
+        {invitation != null && (
+          <div className={cx('invitation-modal', { 'is-cancelled': isInvalid })}>
+            <div className="actions">
+              {isInvalid && (
+                <UiButton onClick={handleCloseCancelled} disabled={isLoading}>
+                  Close
                 </UiButton>
+              )}
 
-                <UiButton onClick={handleAccept} disabled={isLoading} variant="primary">
-                  {state.isAccepting ? 'Accepting' : 'Accept'}
-                </UiButton>
-              </React.Fragment>
-            )}
-          </div>
+              {!isInvalid && (
+                <React.Fragment>
+                  <UiButton onClick={handleDecline} disabled={isLoading}>
+                    {state.isDeclining ? 'Declining...' : 'Decline'}
+                  </UiButton>
 
-          <div className="invitation-modal-card">
-            <div className="user">
-              <div className="avatar">
-                <UiAvatar size="m" img={invitation.sender.avatar} />
+                  <UiButton onClick={handleAccept} disabled={isLoading} variant="primary">
+                    {state.isAccepting ? 'Accepting' : 'Accept'}
+                  </UiButton>
+                </React.Fragment>
+              )}
+            </div>
+
+            <div className="invitation-modal-card">
+              <div className="user">
+                <div className="avatar">
+                  <UiAvatar size="m" img={invitation.sender.avatar} />
+                </div>
+
+                <div className="details">
+                  <h2>{invitation.sender.name}</h2>
+                  <div className="subheading">
+                    <h6 className="ui-subheading">Invited you to watch</h6>
+                  </div>
+                </div>
               </div>
 
-              <div className="details">
-                <h2>{invitation.sender.name}</h2>
-                <div className="subheading">
-                  <h6 className="ui-subheading">Invited you to watch</h6>
+              <div className="thumbnail" style={{ backgroundImage: `url(${invitation.party.video.show.preview_image})` }}>
+                <div className="overlay" />
+                <div className="details">
+                  <div className="tag">
+                    <h6 className="ui-subheading">{getAirDetails(invitation.party.video)}</h6>
+                  </div>
+                  <h3 className="title">{invitation.party.video.show.title}</h3>
                 </div>
               </div>
             </div>
 
-            <div className="thumbnail" style={{ backgroundImage: `url(${invitation.party.video.show.preview_image})` }}>
-              <div className="overlay" />
-              <div className="details">
-                <div className="tag">
-                  <h6 className="ui-subheading">{getAirDetails(invitation.party.video)}</h6>
-                </div>
-                <h3 className="title">{invitation.party.video.show.title}</h3>
-              </div>
+            <div className="invitation-modal-expiration">
+              {!isInvalid && (
+                <h6 className="ui-subheading">
+                  Expires in
+                  <span className="invitation-modal-expiration-lowercaps">
+                    &nbsp;{getFormattedRemainingTime(expiration, now)}
+                  </span>
+                </h6>
+              )}
+
+              {isInvalid && (
+                <h6 className="ui-subheading">
+                  <span className="invitation-modal-expiration-warning-icon">
+                    <i className="fa fa-exclamation-circle" />
+                  </span>
+                  Invitation no longer exists
+                </h6>
+              )}
             </div>
           </div>
+        )}
+      </UiModal>
 
-          <div className="invitation-modal-expiration">
-            {!isInvalid && (
-              <h6 className="ui-subheading">
-                Expires in
-                <span className="invitation-modal-expiration-lowercaps">
-                  &nbsp;{getFormattedRemainingTime(expiration, now)}
-                </span>
-              </h6>
-            )}
-
-            {isInvalid && (
-              <h6 className="ui-subheading">
-                <span className="invitation-modal-expiration-warning-icon">
-                  <i className="fa fa-exclamation-circle" />
-                </span>
-                Invitation no longer exists
-              </h6>
-            )}
-          </div>
-        </div>
-      )}
-    </UiModal>
+      <audio className="invitation-notification-audio" ref={notificationRef}>
+        <source src={asset_invitation} type="audio/ogg" />
+      </audio>
+    </React.Fragment>
   )
 }
 

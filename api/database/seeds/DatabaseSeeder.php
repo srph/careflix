7b487fb8335a68a7f5cd045106435c322e43f880
Kier Borromeo <seraphipod@gmail.com>
2019-06-20 14:24:48 +0800

    Remove seeder

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -13,79 +13,79 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        App\User::truncate();
-        App\Show::truncate();
-        App\ShowGroup::truncate();
-        App\ShowVideo::truncate();
+        // App\User::truncate();
+        // App\Show::truncate();
+        // App\ShowGroup::truncate();
+        // App\ShowVideo::truncate();
 
-        $faker = Faker::create();
+        // $faker = Faker::create();
 
-        $this->command->info('Seeding `users` table (1/1)');
+        // $this->command->info('Seeding `users` table (1/1)');
 
-        App\User::create([
-            'name' => $faker->name,
-            'email' => 'admin@admin.com',
-            'email_verified_at' => now(),
-            'password' => 'password',
-            'remember_token' => Str::random(10),
-            'is_admin' => true
-        ]);
+        // App\User::create([
+        //     'name' => $faker->name,
+        //     'email' => 'admin@admin.com',
+        //     'email_verified_at' => now(),
+        //     'password' => 'password',
+        //     'remember_token' => Str::random(10),
+        //     'is_admin' => true
+        // ]);
 
-        App\User::create([
-            'name' => $faker->name,
-            'email' => 'user@user.com',
-            'email_verified_at' => now(),
-            'password' => 'password',
-            'remember_token' => Str::random(10),
-            'is_admin' => false
-        ]);
+        // App\User::create([
+        //     'name' => $faker->name,
+        //     'email' => 'user@user.com',
+        //     'email_verified_at' => now(),
+        //     'password' => 'password',
+        //     'remember_token' => Str::random(10),
+        //     'is_admin' => false
+        // ]);
 
-        foreach(range(0, 10) as $i) {
-            $is_movie = $i < 5;
+        // foreach(range(0, 10) as $i) {
+        //     $is_movie = $i < 5;
 
-            $title_type = $is_movie ? 'movie' : 'series';
+        //     $title_type = $is_movie ? 'movie' : 'series';
 
-            $this->command->info(sprintf('Seeding `shows` table [%s] (%s / %s)', $title_type, $i + 1, 10));
+        //     $this->command->info(sprintf('Seeding `shows` table [%s] (%s / %s)', $title_type, $i + 1, 10));
 
-            $show = App\Show::create([
-                'title' => $is_movie ? 'Avengers: Endgame' : 'Brooklyn Nine-Nine',
-                'title_type' => $title_type,
-                'synopsis' => $faker->text,
-                'language' => 'English',
-                'air_start' => Carbon::create(2000 + $i, 0, 0),
-                'air_end' => $is_movie ? null : Carbon::create(2000 + $i + 1, 0, 0),
-                'age_rating' => '13+',
-                'preview_image' => 'https://caretv.sgp1.cdn.digitaloceanspaces.com/videos/big-buck-bunny/thumbnail.png'
-            ]);
+        //     $show = App\Show::create([
+        //         'title' => $is_movie ? 'Avengers: Endgame' : 'Brooklyn Nine-Nine',
+        //         'title_type' => $title_type,
+        //         'synopsis' => $faker->text,
+        //         'language' => 'English',
+        //         'air_start' => Carbon::create(2000 + $i, 0, 0),
+        //         'air_end' => $is_movie ? null : Carbon::create(2000 + $i + 1, 0, 0),
+        //         'age_rating' => '13+',
+        //         'preview_image' => 'https://caretv.sgp1.cdn.digitaloceanspaces.com/videos/big-buck-bunny/thumbnail.png'
+        //     ]);
 
-            if ($is_movie) {
-                App\ShowVideo::create([
-                    'show_id' => $show->id,
-                    'video_url' => 'https://caretv.sgp1.cdn.digitaloceanspaces.com/videos/big-buck-bunny/big-buck-bunny.mp4',
-                    'duration' => 596,
-                    'synopsis' => $faker->text,
-                ]);
-            } else {
-                foreach (range(0, 2) as $j) {
-                    $group = App\ShowGroup::create([
-                        'show_id' => $show->id,
-                        'title' => sprintf("Season %s", $j + 1)
-                    ]);
+        //     if ($is_movie) {
+        //         App\ShowVideo::create([
+        //             'show_id' => $show->id,
+        //             'video_url' => 'https://caretv.sgp1.cdn.digitaloceanspaces.com/videos/big-buck-bunny/big-buck-bunny.mp4',
+        //             'duration' => 596,
+        //             'synopsis' => $faker->text,
+        //         ]);
+        //     } else {
+        //         foreach (range(0, 2) as $j) {
+        //             $group = App\ShowGroup::create([
+        //                 'show_id' => $show->id,
+        //                 'title' => sprintf("Season %s", $j + 1)
+        //             ]);
 
-                    foreach(range(0, 5) as $k) {
-                        App\ShowVideo::create([
-                            'show_id' => $show->id,
-                            'show_group_id' => $group->id,
-                            'title' => sprintf("Episode %s", $k),
-                            'video_url' => 'https://caretv.sgp1.cdn.digitaloceanspaces.com/videos/big-buck-bunny/big-buck-bunny.mp4',
-                            'duration' => 596,
-                            'synopsis' => $faker->text,
-                            'preview_image' => 'https://caretv.sgp1.cdn.digitaloceanspaces.com/videos/big-buck-bunny/thumbnail.png'
-                        ]);
-                    }
-                }
-            }
-        }
+        //             foreach(range(0, 5) as $k) {
+        //                 App\ShowVideo::create([
+        //                     'show_id' => $show->id,
+        //                     'show_group_id' => $group->id,
+        //                     'title' => sprintf("Episode %s", $k),
+        //                     'video_url' => 'https://caretv.sgp1.cdn.digitaloceanspaces.com/videos/big-buck-bunny/big-buck-bunny.mp4',
+        //                     'duration' => 596,
+        //                     'synopsis' => $faker->text,
+        //                     'preview_image' => 'https://caretv.sgp1.cdn.digitaloceanspaces.com/videos/big-buck-bunny/thumbnail.png'
+        //                 ]);
+        //             }
+        //         }
+        //     }
+        // }
 
 
         // //////////////////////////////////

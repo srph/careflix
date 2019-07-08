43ad2cbefbc1064529e0ff359c392b63bacc5b3a
srph <seraphipod@gmail.com>
2019-07-09 07:35:44 +0800

    Woarkound type

--- ui/src/components/UiButtonLoader/index.tsx
+++ ui/src/components/UiButtonLoader/index.tsx
@@ -4,6 +4,9 @@ import UiButton, { UiButtonProps } from '~/components/UiButton'
 import UiLoader from '~/components/UiLoader'
 
 type Props = UiButtonProps & {
+  // @TODO We need to remove this. UiButtonProps already has this
+  //  but for some reason our typcheck won't work without this.
+  disabled?: boolean
   isLoading?: boolean
 }
 

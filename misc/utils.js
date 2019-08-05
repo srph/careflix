459cd6fbf83512647cb08771bf167a15c66b044f
Kier Borromeo <seraphipod@gmail.com>
2019-08-06 05:08:12 +0800

    Fix misc utils

--- misc/utils.js
+++ misc/utils.js
@@ -1,22 +1,24 @@
-export function last(arr) {
+const path = require('path')
+
+function last(arr) {
   return arr[arr.length - 1]
 }
 
 // Get dir/filename from path
 // /home/srph/uploads/tarzan-x -> tarzan-x
-export function name(file) {
+exports.name = function name(file) {
   return last(file.split(path.sep))
 }
 
 // Naturally sort alphanumerical strings
 // Without this, we'll get [ep1, ep10, ep2] instead of [ep1, ep2, ep10]
 // https://stackoverflow.com/questions/2802341/javascript-natural-sort-of-alphanumerical-strings
-export function sort(files) {
+exports.sort = function sort(files) {
   var collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
   return files.sort(collator.compare)
 }
 
 // Remove trailing slashes
-export function untrail(path) {
-  return path.replace(/\/$/, '')
+exports.untrail = function untrail(dir) {
+  return dir.replace(/\/$/, '')
 }

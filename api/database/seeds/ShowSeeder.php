d1e186bdd658c84510d1466296fee99a4f4f08e9
Kier Borromeo <seraphipod@gmail.com>
2019-08-10 06:42:51 +0800

    Update list

--- api/database/seeds/ShowSeeder.php
+++ api/database/seeds/ShowSeeder.php
@@ -1502,7 +1502,7 @@ class ShowSeeder extends Seeder
             [
                 'title' => 'Bungou Stray Dogs',
                 'title_type' => 'series',
-                'synopsis' => 'Nakajima Atsushi was kicked out of his orphanage, and now he has no place to go and no food. While he is standing by a river, on the brink of starvation, he rescues a man whimsically attempting suicide. That man is Dazai Osamu, and he and his partner Kunikida are members of a very special detective agency. They have supernatural powers and deal with cases that are too dangerous for the police or the military. They\'re tracking down a tiger that has appeared in the area recently, around the time Atsushi came to the area. The tiger seems to have a connection to Atsushi, and by the time the case is solved, it is clear that Atsushi\'s future will involve much more of Dazai and the rest of the detectives.',
+                'synopsis' => 'Nakajima Atsushi meets Dazai Osamu, a member of a very special detective agency with supernatural powers. After their encounter, he becomes involved in solving their dangerous special mystery cases.',
                 'language' => 'Japanese',
                 'air_start' => Carbon::create(2016, 4, 7),
                 'air_end' => Carbon::create(2019, 6, 28),
@@ -1510,6 +1510,13 @@ class ShowSeeder extends Seeder
                 'age_rating' => 'PG-13',
                 //
                 'seasons' => [
+                    [
+                        'title' => 'Season 1',
+                        'episodes' => 12,
+                        'extension' => 'mp4',
+                        'subtitle_language' => 'en',
+                        'duration' => '23:40'
+                    ],
                     [
                         'title' => 'Season 2',
                         'episodes' => 12,
@@ -1526,6 +1533,19 @@ class ShowSeeder extends Seeder
                     ]
                 ]
             ],
+            // [
+            //     'title' => 'Aladdin',
+            //     'title_type' => 'movie',
+            //     'synopsis' => 'A kind-hearted street urchin and a power-hungry Grand Vizier vie for a magic lamp that has the power to make their deepest wishes come true.',
+            //     'language' => 'English',
+            //     'preview_image' => Helper::getPreviewUrlFromMovieTitle('Aladdin'),
+            //     'air_start' => Carbon::create(2019, 5, 24),
+            //     'age_rating' => 'PG',
+            //     'subtitle_url' => Helper::getSubtitleUrlFromMovieTitle('Aladdin', 'en'),
+            //     //
+            //     'extension' => 'mp4',
+            //     'duration' => Helper::getDurationInSecondsFromReadableFormat('2:08:59')
+            // ],
         ];
 
         foreach($movies as $movie) {
